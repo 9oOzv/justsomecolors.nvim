61f4ed2ffe45529ba2cccbc0176d1afffe13789e
Miika Piiroinen <miika.piiroinen@aalto.fi>
2023-03-10 16:02:12 +0200

    Update some colors. Add NeoTree colors.

--- lua/justsomeneoncolorscheme/justsomeneoncolorscheme.lua
+++ lua/justsomeneoncolorscheme/justsomeneoncolorscheme.lua
@@ -9,6 +9,7 @@ local c0 = hsluv(190, 100, 90)
 local c1 = hsluv(317, 100, 58)
 local c2 = hsluv(143, 100, 87)
 local red = hsluv(0, 100, 55)
+local yellow = hsluv(88, 100, 81)
 local cmid = fg.da(25)
 local clow = cmid.da(50)
 local cverylow = clow.da(30)
@@ -18,14 +19,14 @@ local theme = lush(function()
   return {
     base { bg = bg, fg = fg },
     undef { bg = red, fg = opaque },
-    error { bg = opaque, fg = red},
+    error { bg = opaque, fg = red },
     mid { bg = bg, fg = cmid },
     low { bg = bg, fg = clow },
     verylow { bg = bg, fg = cverylow },
 
     Normal          { base },
     Cursor          { bg = opaque.mix(fg, 30) }, -- Screen-line at the cursor, when 'cursorline' is set.  Low-priority if foreground (ctermfg OR guifg) is not set.
-    CursorLine      { bg = opaque.mix(fg, 8) }, -- Screen-line at the cursor, when 'cursorline' is set.  Low-priority if foreground (ctermfg OR guifg) is not set.
+    CursorLine      { bg = opaque.mix(fg, 4) }, -- Screen-line at the cursor, when 'cursorline' is set.  Low-priority if foreground (ctermfg OR guifg) is not set.
     CursorColumn    { CursorLine },
     Visual          { bg = opaque.mix(fg, 15) },
     Whitespace      { verylow, gui = "bold" },
@@ -36,6 +37,18 @@ local theme = lush(function()
     Search          { bg = c2, fg = opaque.mix(fg, 20) },
     IncSearch       { bg = Search.bg.rotate(-20), fg = Search.fg.li(10) },
 
+    NormalFloat     { base, bg = opaque.mix(fg, 3), fg = c0 }, -- Normal text in floating windows.
+    DiffAdd         { base, fg = c2 }, -- diff mode: Added line |diff.txt|
+    DiffChange      { base, fg = c1}, -- diff mode: Changed line |diff.txt|
+    DiffDelete      { base, fg = red }, -- diff mode: Deleted line |diff.txt|
+    ErrorMsg        { error }, -- error messages on the command line
+    SignColumn      { mid }, -- 'foldcolumn'
+    MatchParen      { base, bg = c1, fg = c0, gui = "bold" }, -- The character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
+    Pmenu           { base, fg = c0 }, -- Popup menu: normal item.
+    PmenuSel        { base, fg = c1 }, -- Popup menu: selected item.
+    PmenuSbar       { bg = c0, fg = opaque }, -- Popup menu: scrollbar.
+    PmenuThumb      { bg = opaque, fg = c0 }, -- Popup menu: Thumb of the scrollbar.
+
     Constant        { base, fg = c1.de(30) }, -- (preferred) any constant
     Identifier      { base, fg = c0 }, -- (preferred) any variable name
     Statement       { base, fg = c1, gui = "bold" }, -- (preferred) any statement
@@ -44,28 +57,74 @@ local theme = lush(function()
     Type            { base, fg = c0, gui = "bold" }, -- (preferred) int, long, char, etc.
     Special         { mid }, -- (preferred) any special symbol
 
-    NormalFloat     { base, bg = opaque.mix(fg, 3), fg = c0 }, -- Normal text in floating windows.
-    DiffAdd         { base, fg = c2 }, -- diff mode: Added line |diff.txt|
-    DiffChange      { base, fg = c1}, -- diff mode: Changed line |diff.txt|
-    DiffDelete      { base, fg = red }, -- diff mode: Deleted line |diff.txt|
-
-    ErrorMsg        { error }, -- error messages on the command line
     Todo            { bg = opaque.mix(fg, 5), fg = c1, gui = "bold" }, -- (preferred) anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+
+    DiagnosticVirtualTextError                 { fg = cverylow.saturation(red.s).hue(red.h) },
+    DiagnosticVirtualTextWarn                 { fg = clow.saturation(yellow.s).hue(yellow.h) },
+    DiagnosticVirtualTextInfo                 { fg = cverylow },
+    DiagnosticVirtualTextHint                 { fg = cverylow },
+    DiagnosticSignError                 { fg = cverylow.saturation(red.s).hue(red.h) },
+    DiagnosticSignWarn                 { fg = clow.saturation(yellow.s).hue(yellow.h) },
+    DiagnosticSignInfo                 { fg = cverylow },
+    DiagnosticSignHint                 { fg = cverylow },
 
     NotifyBackground { bg = opaque }, -- Some box appeared telling me to define this
 
-    Pmenu        { base, fg = c0 }, -- Popup menu: normal item.
-    PmenuSel     { base, fg = c1 }, -- Popup menu: selected item.
-    PmenuSbar    { bg = c0, fg = opaque }, -- Popup menu: scrollbar.
-    PmenuThumb   { bg = opaque, fg = c0 }, -- Popup menu: Thumb of the scrollbar.
+    -- NeoTreeNormal               { low },
+    -- NeoTreeNormalNC             { low },
+    -- NeoTreeSignColumn           { low },
+    -- NeoTreeStatusLine           { low },
+    -- NeoTreeStatusLineNC         { low },
+    -- NeoTreeVertSplit            { low },
+    -- NeoTreeWinSeparator         { low },
+    -- NeoTreeEndOfBuffer          { low },
+    -- NeoTreeFloatBorder          { low },
+    -- NeoTreeFloatNormal          { low },
+    -- NeoTreeFloatTitle           { low },
+    -- NeoTreeTitleBar             { low },
+    -- NeoTreeBufferNumber         { low },
+    -- NeoTreeDimText              { low },
+    -- NeoTreeMessage              { low },
+    -- NeoTreeFadeText1            { low },
+    -- NeoTreeFadeText2            { low },
+    -- NeoTreeDotfile              { low },
+    -- NeoTreeHiddenByName         { low },
+    -- NeoTreeCursorLine           { low },
+    -- NeoTreeDirectoryName        { low },
+    -- NeoTreeDirectoryIcon        { low },
+    -- NeoTreeFileIcon             { low },
+    -- NeoTreeFileName             { low },
+    -- NeoTreeFileNameOpened       { low },
+    -- NeoTreeSymbolicLinkTarget   { low },
+    -- NeoTreeFilterTerm           { low },
+    -- NeoTreeRootName             { low },
+    -- NeoTreeIndentMarker         { low },
+    -- NeoTreeExpander             { low },
+    -- NeoTreeModified             { low },
+    -- NeoTreeWindowsHidden        { low },
+    -- NeoTreePreview              { low },
+    -- NeoTreeGitAdded             { low },
+    -- NeoTreeGitDeleted           { low },
+    -- NeoTreeGitModified          { low },
+    -- NeoTreeGitConflict          { low },
+    -- NeoTreeGitIgnored           { low },
+    -- NeoTreeGitRenamed           { low },
+    -- NeoTreeGitStaged            { low },
+    -- NeoTreeGitUnstaged          { low },
+    -- NeoTreeGitUntracked         { low },
+    -- NeoTreeTabActive            { low },
+    -- NeoTreeTabInactive          { low },
+    -- NeoTreeTabSeparatorActive   { low },
+    -- NeoTreeTabSeparatorInactive { low },
 
-    -- ColorColumn  { }, -- used for the columns set with 'colorcolumn'
     -- NormalFloat  { }, -- Normal text in floating windows.
+    -- ColorColumn  { }, -- used for the columns set with 'colorcolumn'
     -- Conceal      { }, -- placeholder characters substituted for concealed text (see 'conceallevel')
     -- Cursor       { }, -- character under the cursor
     -- lCursor      { }, -- the character under the cursor when |language-mapping| is used (see 'guicursor')
     -- CursorIM     { }, -- like Cursor, but used when in IME mode |CursorIM|
-    -- Directory    { }, -- diff mode: Added line |diff.txt|
+    -- Directory    { }, -- directory names (and other special names in listings)
+    -- DiffAdd      { }, -- diff mode: Added line |diff.txt|
     -- DiffChange   { }, -- diff mode: Changed line |diff.txt|
     -- DiffDelete   { }, -- diff mode: Deleted line |diff.txt|
     -- DiffText     { }, -- diff mode: Changed text within a changed line |diff.txt|
@@ -85,6 +144,10 @@ local theme = lush(function()
     -- MoreMsg      { }, -- |more-prompt|
     -- NonText      { }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
     -- NormalNC     { }, -- normal text in non-current windows
+    -- Pmenu        { }, -- Popup menu: normal item.
+    -- PmenuSel     { }, -- Popup menu: selected item.
+    -- PmenuSbar    { }, -- Popup menu: scrollbar.
+    -- PmenuThumb   { }, -- Popup menu: Thumb of the scrollbar.
     -- Question     { }, -- |hit-enter| prompt and yes/no questions
     -- QuickFixLine { }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
     -- SpecialKey   { }, -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' whitespace. |hl-Whitespace| 
@@ -98,8 +161,10 @@ local theme = lush(function()
     -- TabLineFill  { }, -- tab pages line, where there are no labels
     -- TabLineSel   { }, -- tab pages line, active tab page label
     -- Title        { }, -- titles for output from ":set all", ":autocmd" etc.
+    -- Visual       { }, -- Visual mode selection
     -- VisualNOS    { }, -- Visual mode selection when vim is "Not Owning the Selection".
     -- WarningMsg   { }, -- warning messages
+    -- Whitespace   { }, -- "nbsp", "space", "tab" and "trail" in 'listchars'
     -- WildMenu     { }, -- current match in 'wildmenu' completion
 
     -- These groups are not listed as default vim groups,
